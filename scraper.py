a2cca3e34abd1b2df69269258cb14fd988cc4860
reneczka <46569696+reneczka@users.noreply.github.com>
2024-07-17 20:21:04 +0200

    print whether job is remote

--- scraper.py
+++ scraper.py
@@ -2,15 +2,20 @@ from playwright.sync_api import sync_playwright
 import time
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=500)
+    # browser = p.chromium.launch(headless=False, slow_mo=500)
+    browser = p.chromium.launch(slow_mo=250)
     page = browser.new_page()
     page.goto('https://justjoin.it')
-    
+
     page.wait_for_selector("#cookiescript_accept")
+
     page.click("#cookiescript_accept")
-    page.click('a[href="/all-locations/python"]')    # select all elements with the tag a which has an attribute href with a value of all locations python
+    page.click('a[href="/all-locations/python"]')
     page.click('button[name="sort_filter_button"]')  
-    page.get_by_text("Latest").click()
+
+    # page.get_by_text("Latest").click()
+    page.get_by_text("Lowest").click()
+    
     page.wait_for_load_state('networkidle')
 
     job_anchor = page.locator('a.offer_list_offer_link').first
@@ -22,19 +27,22 @@ with sync_playwright() as p:
     job_name_text = job_name.text_content()
     print(f"Job Name: {job_name_text}")
 
-    job_name_parent = job_name.locator('..')
+    container_company_location_remote = job_name.locator('..').locator("xpath=following-sibling::*[1]")
 
-    job_name_parent_sibling = job_name_parent.locator("xpath=following-sibling::*[1]")
-
-    company_name_text = job_name_parent_sibling.locator('span').first.text_content()
+    company_name_text = container_company_location_remote.locator('span').first.text_content()
     print(f"Company Name: {company_name_text}")
 
-    job_location_text = job_name_parent_sibling.locator('span').nth(1).text_content()
+    job_location_text = container_company_location_remote.locator('span').nth(1).text_content()
     print(f"Job Location: {job_location_text}")
 
+    remote_tag = container_company_location_remote.get_by_text('remote')
 
 
+    is_remote = True if remote_tag.count() > 0 else False
 
+    print(f"Is Remote: {is_remote}")
+
+ 
 
 
 time.sleep(5)
